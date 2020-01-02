abf77f7cf87961884cff342c335bec938810175a
dikelongzai <15399073387@163.com>
2020-01-02 13:15:41 +0800

    DB ACID

--- db/src/test/java/DBConnectionFactoryTest.java
+++ db/src/test/java/DBConnectionFactoryTest.java
@@ -1,3 +1,4 @@
+import com.dragon.web.common.security.Log;
 import com.dragon.web.db.util.*;
 import org.junit.Assert;
 import org.junit.Before;
@@ -24,7 +25,21 @@ public class DBConnectionFactoryTest {
     public static final String sqlP = "select * FROM PO_MENU WHERE menuname={} ";
     public static final String sql_MAPPARAM = "select * FROM PO_MENU WHERE menuname={menuname}";
     public static final String sqlP_N = "select * FROM PO_MENU WHERE menuname={menuname} AND menuid={n:menuid}";
+    public static final String sqlP_SYSDATE = "select * FROM PO_NEWS WHERE date<{dt:newsdate}";
+    public static final String sqlP_SYSDAT = "select * FROM PO_NEWS WHERE date<{dt:sysdate}";
 
+    public static final String sqlP_SYSTIME = "select * FROM PO_NEWS WHERE date<{dt:sysdatetime}";
+    public static final String CREATE_SQL="(`runoob_id` INT UNSIGNED AUTO_INCREMENT,\n" +
+            "   `runoob_title` VARCHAR(100) NOT NULL,\n" +
+            "   `runoob_author` VARCHAR(40) NOT NULL,\n" +
+            "   `submission_date` DATE,\n" +
+            "   PRIMARY KEY ( `runoob_id` ))";
+
+    public static final String INSERT_SQL="INSERT INTO TEMP_po_tmp_table" +
+            "    (runoob_title, runoob_author, submission_date)" +
+            "    VALUES" +
+            "    ({runoob_title}, {runoob_author}, NOW());";
+    public static final String UPDATE_sQL="update TEMP_po_tmp_table set runoob_title={runoob_title},runoob_author={runoob_author} WHERE RUNOOB_ID={n:RUNOOB_ID}";
     //    public ;
     @Before
     public void testInit() {
@@ -39,6 +54,7 @@ public class DBConnectionFactoryTest {
         DBConnectionFactory dbConnectionFactory = new C3P0Factory();
         dbConnectionFactory.init(propertiesInitDB, DBConstant.DEFAULT_DSNAME);
         DataSource.addDBConnectionFactory(DBConstant.DEFAULT_DSNAME, dbConnectionFactory);
+        dbConnection = DataSource.getDBConnection(DBConstant.DEFAULT_DSNAME);
     }
 
     @Test
@@ -63,7 +79,6 @@ public class DBConnectionFactoryTest {
         Assert.assertEquals(dbConnection.queryBySql(sqlN, map).size(), 1);
         List list = new ArrayList();
         list.add("个人办公");
-
         Assert.assertEquals(dbConnection.queryBySql(sqlP, list).size(), 1);
         map = new HashMap();
         map.put("menuname", "个人办公");
@@ -72,9 +87,44 @@ public class DBConnectionFactoryTest {
         map = new HashMap();
         map.put("menuname", "个人办公");
         map.put("menuid", 3);
-        Assert.assertEquals(dbConnection.queryBySql(sqlP_N, map).size(), 1);
-
-
+        map.put("newsdate", "2019-12-31");
 //        Assert.assertEquals(dbConnection.queryBySql(sqlP_N, map).size(), 1);
+        map = new HashMap();
+        map.put("menuname", "个人办公");
+        map.put("menuid", 3);
+        map.put("newsdate", "2019-12-31");
+        Assert.assertEquals(dbConnection.queryBySql(sqlP_SYSDATE, map).size(), 1);
+        Assert.assertEquals(dbConnection.queryBySql(sqlP_SYSDAT, map).size(), 1);
+        Assert.assertEquals(dbConnection.queryBySql(sqlP_SYSTIME, null).size(), 1);
+
+    }
+    @Test
+    public void testCreateDropTable()throws Exception{
+        dbConnection.dropTempTable("TEMP_po_tmp_table");
+        String tableName=dbConnection.createTempTable("po_tmp_table",CREATE_SQL,false,false);
+        Log.debug("success create table "+tableName);
+
+    }
+    @Test
+    public void testUpdate()throws Exception{
+        Map map = new HashMap();
+        map.put("runoob_title", "runoob_title");
+        map.put("runoob_author", "runoob_author");
+        map.put("RUNOOB_ID", 1);
+
+//
+//        dbConnection.updateBySql(INSERT_SQL,map);
+        List<Map<String,String>> list=dbConnection.queryBySql("SELECT * FROM TEMP_po_tmp_table");
+
+        list.forEach(maptmp->{
+            for(Map.Entry entry:maptmp.entrySet()){
+                System.out.print("K:"+entry.getKey().toString()+";V:"+entry.getValue().toString());
+            }
+            System.out.println("");
+        });
+        map.put("runoob_title", "runoob_title_update");
+        map.put("runoob_author", "runoob_author");
+        map.put("RUNOOB_ID", 1);
+        dbConnection.updateBySql(UPDATE_sQL,map);
     }
 }
